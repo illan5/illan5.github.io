5532a19e170cb17c40fba152d7b1d4ec3c727dac
Jose Illan <jose_illan@hotmail.com>
2026-04-23 13:18:51 +0200

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://illan5.github.io',
-  base: '/illan5',
+  site: 'https://www.joseillan.com',
 });
